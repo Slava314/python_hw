369d856389e27ae2e05217dbdfb4df97e048442b
Slava314 <vorobev.stu@gmail.com>
2022-01-28 02:50:54 +0300

    graph for medium

--- hw1/main.py
+++ hw1/main.py
@@ -1,7 +1,6 @@
 from fib import fib
 import ast
 import networkx as nx
-import matplotlib.pyplot as plt
 import inspect
 
 
@@ -19,7 +18,7 @@ node_names = {}
 
 def visit_node(gv, node, name):
 	counter = next(gen_num)
-	node_names[counter] = name
+	node_names[counter] = str(counter) + ': ' + name
 	g.add_node(counter)
 	lst = gv.generic_visit(node)
 	g.add_edges_from(map(lambda x: (counter, x), lst))
@@ -28,23 +27,53 @@ def visit_node(gv, node, name):
 
 class GraphVisitor(ast.NodeVisitor):
 
-	def visit_Module(self, node: ast.Module):
-		return visit_node(self, node, 'module')
-
-	def visit_FunctionDef(self, node: ast.FunctionDef):
+	def visit_FunctionDef(self, node):
 		return visit_node(self, node, "function '{}'".format(node.name))
 
-	def visit_arguments(self, node: ast.arguments):
+	def visit_arguments(self, node):
 		return visit_node(self, node, 'arguments')
 
-	def visit_For(self, node: ast.For):
+	def visit_For(self, node):
 		return visit_node(self, node, 'for cycle')
 
 	def visit_Return(self, node):
 		return visit_node(self, node, 'return')
 
-	def visit_Name(self, node: ast.Name):
+	def visit_Name(self, node):
 		return visit_node(self, node, "variable '{}'".format(node.id))
+
+	def visit_BinOp(self, node: ast.BinOp):
+		return visit_node(self, node, 'bin op')
+
+	def visit_Add(self, node):
+		return visit_node(self, node, 'add')
+
+	def visit_Assign(self, node):
+		return visit_node(self, node, 'assign')
+
+	def visit_arg(self, node):
+		return visit_node(self, node, "variable '{}'".format(node.arg))
+
+	def visit_List(self, node):
+		return visit_node(self, node, 'list')
+
+	def visit_Constant(self, node):
+		return visit_node(self, node, "constant '{}'".format(node.value))
+
+	def visit_Expr(self, node: ast.Expr):
+		return visit_node(self, node, 'expr')
+
+	def visit_Call(self, node: ast.Call):
+		return visit_node(self, node, 'call')
+
+	def visit_Sub(self, node: ast.Sub):
+		return visit_node(self, node, 'sub')
+
+	def visit_Attribute(self, node: ast.Attribute):
+		return visit_node(self, node, 'attribute')
+
+	def visit_Subscript(self, node: ast.Subscript):
+		return visit_node(self, node, 'subscript')
 
 	def generic_visit(self, node):
 		lst = []
@@ -70,7 +99,6 @@ def main():
 	g1 = nx.relabel_nodes(g, node_names)
 	g2 = nx.dfs_tree(g1)
 	g3 = nx.drawing.nx_agraph.to_agraph(g2)
-	#subax1 = plt.subplot(121)
 	pos = g3.layout('dot')
 	g3.draw('artifacts/AST.png', format='png')
 	#nx.draw(g2, with_labels=True)
