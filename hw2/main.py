2d8a9a8c820561ea2e1f7aae6276f8eb6c902a47
Slava314 <vorobev.stu@gmail.com>
2022-02-16 01:20:17 +0300

    add easy_task

---
+++ hw2/main.py
@@ -0,0 +1,32 @@
+def make_tex_file(name_of_file, string):
+    f = open('artifacts/' + name_of_file + '.tex', 'w')
+    head = "\\documentclass[12pt]{article}\n\\usepackage[T2A]{fontenc}\n\\usepackage[utf8]{inputenc}\n\\usepackage[" \
+           "english,russian]{babel}\n\\usepackage{amsmath,amsfonts,amssymb,amsthm,mathtools}\n\\begin{document}\n "
+    end = "\end{document}\n"
+    f.write(head + string + end)
+    f.close()
+
+
+def resize_lst(lst, n, element=''):
+    return lst[:n] + [element for _ in range(n - len(lst))]
+
+
+def easy_task(lst):
+    list_str = "\\begin{tabular}{ |"
+    n = max(map(len, lst))
+    lst = list(map(lambda x: resize_lst(x, n), lst))
+    list_str += "c|" * n + '}\n'
+    list_str += ''.join(map(lambda x: '\\hline\n' + ''.join(map(lambda y: str(y) + ' & ', x))[:-2] + ' \\\\\n', lst))
+    list_str += "\\hline\n\\end{tabular}\n"
+    make_tex_file('easy_task', list_str)
+
+
+def main():
+    n = 7
+    lst = [[x * y for x in range(1, n)] for y in range(1, 2 * n)]
+    lst.append(['help'] * 10)
+    easy_task(lst)
+
+
+if __name__ == "__main__":
+    main()
