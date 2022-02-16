63731fad9d68c0c48a23b41bce31ba69a1d2e753
Slava314 <vorobev.stu@gmail.com>
2022-02-16 13:58:28 +0300

    fixes

--- hw2/main.py
+++ hw2/main.py
@@ -1,12 +1,14 @@
 from ExampleGeneratorOfAST import generate_image
 from ExampleGeneratorOfAST import fib
 
+from functools import reduce
+
 
 def make_tex_file(name_of_file, string):
     f = open('artifacts/' + name_of_file + '.tex', 'w')
-    head = "\\documentclass[12pt]{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[" \
-           "english]{babel}\n\\usepackage{graphicx}\n\\graphicspath{ {./artifacts/} }\n\\begin{document}\n"
-    end = "\end{document}\n"
+    head = '\\documentclass[12pt]{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[' \
+           'english]{babel}\n\\usepackage{graphicx}\n\\graphicspath{ {./artifacts/} }\n\\begin{document}\n'
+    end = '\end{document}\n'
     f.write(head + string + end)
     f.close()
 
@@ -16,12 +18,15 @@ def resize_lst(lst, n, element=''):
 
 
 def easy_task(lst):
-    list_str = "\\begin{tabular}{ |"
+    list_str = '\\begin{tabular}{ |'
     n = max(map(len, lst))
     lst = list(map(lambda x: resize_lst(x, n), lst))
-    list_str += "c|" * n + '}\n'
-    list_str += ''.join(map(lambda x: '\\hline\n' + ''.join(map(lambda y: str(y) + ' & ', x))[:-2] + ' \\\\\n', lst))
-    list_str += "\\hline\n\\end{tabular}\n"
+    list_str += 'c|' * n + '}\n\\hline\n'
+    list_str += reduce(
+        lambda l, r: l + '\\hline\n' + r,
+        map(lambda line: reduce(lambda l, r: str(l) + ' & ' + str(r), line) + ' \\\\\n', lst)
+    )
+    list_str += '\\hline\n\\end{tabular}\n'
     # make_tex_file('easy_task', list_str)
     return list_str
 
@@ -35,10 +40,10 @@ def medium_task(picture_path, lst):
 
 def main():
     n = 7
-    lst = [[x * y for x in range(1, n)] for y in range(1, 2 * n)]
-    lst.append(['help'] * 10)
+    lst = [[str(x * y) for x in range(1, n)] for y in range(1, 2 * n)]
+    lst.append(['help' + str(i) for i in range(10)])
     medium_task('AST.png', lst)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
