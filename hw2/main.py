cb0f91122044cbcf34a7ae30dfa09e358997b5d9
Slava314 <vorobev.stu@gmail.com>
2022-02-16 12:18:45 +0300

    add medium_task with package and artifacts

--- hw2/main.py
+++ hw2/main.py
@@ -1,3 +1,7 @@
+from ExampleGeneratorOfAST import generate_image
+from ExampleGeneratorOfAST import fib
+
+
 def make_tex_file(name_of_file, string):
     f = open('artifacts/' + name_of_file + '.tex', 'w')
     head = "\\documentclass[12pt]{article}\n\\usepackage[T2A]{fontenc}\n\\usepackage[utf8]{inputenc}\n\\usepackage[" \
@@ -18,11 +22,14 @@ def easy_task(lst):
     list_str += "c|" * n + '}\n'
     list_str += ''.join(map(lambda x: '\\hline\n' + ''.join(map(lambda y: str(y) + ' & ', x))[:-2] + ' \\\\\n', lst))
     list_str += "\\hline\n\\end{tabular}\n"
-    make_tex_file('easy_task', list_str)
+    # make_tex_file('easy_task', list_str)
+    return list_str
 
 
-def medium_task(picture_path):
-    list_str = '\\includegraphics[scale=0.25]{' + picture_path + '}\n'
+def medium_task(picture_path, lst):
+    list_str = easy_task(lst)
+    generate_image.generate_image(fib.fib)
+    list_str += '\n\\includegraphics[scale=0.25]{' + picture_path + '}\n'
     make_tex_file('medium_task', list_str)
 
 
@@ -30,8 +37,7 @@ def main():
     n = 7
     lst = [[x * y for x in range(1, n)] for y in range(1, 2 * n)]
     lst.append(['help'] * 10)
-    easy_task(lst)
-    medium_task('AST.png')
+    medium_task('AST.png', lst)
 
 
 if __name__ == "__main__":
