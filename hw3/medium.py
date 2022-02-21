6660304b3fea8938649fec91a3ec13092cfa0ff4
Slava314 <vorobev.stu@gmail.com>
2022-02-21 16:52:20 +0300

    add medium.py

---
+++ hw3/medium.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+class FileMix:
+    def write_to_file(self, file):
+        file.write(str(self))
+
+
+class StrMix:
+    def __str__(self):
+        return 'matrix: {} * {}\n'.format(self.n, self.m) + str(self.val)
+
+
+class GetSetMix:
+    def get_val(self):
+        return self.val
+
+    def get_n(self):
+        return self.n
+
+    def get_m(self):
+        return self.m
+
+    def set_val(self, value):
+        self.val = value
+        self.n = len(value)
+        self.m = len(value[0])
+
+
+class Matrix:
+    def __init__(self, lst):
+        self.val = lst
+        self.n = len(self.val)
+        self.m = len(self.val[0])
+
+
+class MatrixMix(Matrix, np.lib.mixins.NDArrayOperatorsMixin, FileMix, StrMix, GetSetMix):
+    _HANDLED_TYPES = (Matrix,)
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        out = kwargs.get('out', ())
+        for x in inputs + out:
+            if not isinstance(x, self._HANDLED_TYPES + (Matrix,)):
+                return NotImplemented
+
+        inputs = tuple(x.val if isinstance(x, Matrix) else x
+                       for x in inputs)
+        if out:
+            kwargs['out'] = tuple(
+                x.val if isinstance(x, Matrix) else x
+                for x in out)
+        result = getattr(ufunc, method)(*inputs, **kwargs)
+
+        if type(result) is tuple:
+            return tuple(type(self)(x) for x in result)
+        elif method == 'at':
+            return None
+        else:
+            return type(self)(result)
+
+
+np.random.seed(0)
+a = MatrixMix(np.random.randint(0, 10, (10, 10)))
+b = MatrixMix(np.random.randint(0, 10, (10, 10)))
+
+with open("./artifacts/medium/matrix+.txt", 'w') as f:
+    (a + b).write_to_file(f)
+
+with open("./artifacts/medium/matrix*.txt", 'w') as f:
+    (a * b).write_to_file(f)
+
+with open("./artifacts/medium/matrix@.txt", 'w') as f:
+    (a @ b).write_to_file(f)
